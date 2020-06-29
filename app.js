aad15f7c9797522335d0286cf2c500cbe8da79a6
Firdaus Helmy <46524543+firdaushelmy@users.noreply.github.com>
2020-06-30 01:13:59 +0800

    init bodyParser, ejs, mongoose

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const mongoose = require('mongoose');
+
 const app = express();
-app.set('view engine', 'ejs')
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 const port = process.env.PORT || 3000;
